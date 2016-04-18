3481584cb2ac8dd50da3e854777f30957d4f1e60
nvankems <nvankems@yahoo.com>
2016-04-18 14:43:14 -0500

    first heroku commit

--- ActiveUnitsSchema.js
+++ ActiveUnitsSchema.js
@@ -3,51 +3,36 @@ let Schema = mongoose.Schema;
 
 let Data = new Schema(
 {
-    waterTemp: { type: Number, required: true },
-    airTemp: { type: Number, required: true },
-    humidity: { type: Number, require: true },
-    light: { type: Number, require: true }
+    waterTemp : { type: Number, required: true },
+    airTemp : { type: Number, required: true },
+    ph : { type: Number, required: true },
+    humidity : { type: Number, require: true },
+    light : { type: Number, require: true }
+
 });
 
 let Hour = new Schema(
 {
-<<<<<<< HEAD
-    data: [ Data ]
-=======
     data : [ Data ]
->>>>>>> 57a67d6e30331c6a38b8386b8f72f5516211f29a
 });
 
 let Day = new Schema(
 {
-<<<<<<< HEAD
-    hour: [ Hour ]
-=======
     hour : [ Hour ]
->>>>>>> 57a67d6e30331c6a38b8386b8f72f5516211f29a
 });
 
 let Units = new Schema(
 {
-    name: { type: String, required: true },
-    product: { type: String, required: true },
+    name: { type: String, required: true, default: "Unit00"},
+    product: { type: String, required: true, default: "Micro Greens"},
     harvest: {
         id: { type: Number, required: true},
         startTime: { type: Date, default: Date.now }
-<<<<<<< HEAD
-    }, 
-
-    day: [ Day ]
-});
-
-var ActiveUnits = new Schema(
-=======
     },
     day: [ Day ]
 });
 
 let Active = new Schema(
->>>>>>> 57a67d6e30331c6a38b8386b8f72f5516211f29a
 {
     numUnits: { type: Number, required: true },
     units: [ Units ]  
---
+++ public/js/app-compiled.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('DataGrow', ['ui.router']);
+
+//# sourceMappingURL=app-compiled.js.map
