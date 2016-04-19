ec092681ada3c056314d3967cd32a2b5cd46a00b
nvankems <nvankems@yahoo.com>
2016-04-19 10:45:42 -0500

    service set-up

--- public/js/websocketService.js
+++ public/js/websocketService.js
@@ -2,6 +2,8 @@ angular.module('DataGrow').service('websocketService', function( $http, $q ) {
 
     let ws = new WebSocket('ws://localhost:8000');
 
+    console.log("Hello, World!");
+
     setInterval(function() {
         console.log(ws.readyState)
     }, 5000);
