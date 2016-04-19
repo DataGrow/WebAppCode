e36967b35b2a35886f3780cb61a45d174bf6899f
nvankems <nvankems@yahoo.com>
2016-04-19 11:05:04 -0500

    websocket testing

--- public/js/websocketService.js
+++ public/js/websocketService.js
@@ -2,6 +2,14 @@ angular.module('DataGrow').service('websocketService', function( $http, $q ) {
 
     let ws = new WebSocket('ws://localhost:8000');
 
-    
+    ws.onopen = function() {
+        ws.send("Hello, World!");
+        alert("Message is sent...")
+    };
+
+    ws.onmessage = function (evt) {
+        var received = evt.data;
+        alert("received message: " + received);
+    }
 
 });
