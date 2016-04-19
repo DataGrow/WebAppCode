9dbc67981329fd55d1b3c2c272cfb517975b9684
nvankems <nvankems@yahoo.com>
2016-04-19 10:42:30 -0500

    adding websocket service

--- public/js/websocketService.js
+++ public/js/websocketService.js
@@ -1,3 +1,9 @@
 angular.module('DataGrow').service('websocketService', function( $http, $q ) {
-    
+
+    let ws = new WebSocket('ws://localhost:8000');
+
+    setInterval(function() {
+        console.log(ws.readyState)
+    }, 5000);
+
 });
