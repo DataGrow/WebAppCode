262e9d707ca5da72b7b38ec5ac4603c5df6e9a55
Justin Bennett <fortitude1981@gmail.com>
2016-04-19 17:51:41 -0500

    updated gulp - not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,60 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var runSequence = require('run-sequence');
 
-gulp.task('webserver', function() {
-  connect.server();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'development'
+    },
+  })
 });
-
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+  return gulp.src('development/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
  
-gulp.task('watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch('development/styles/**/*.scss', ['sass']);
+  gulp.watch('development/*.html', browserSync.reload); 
+  gulp.watch('development/js/**/*.js', browserSync.reload); 
 });
 
-gulp.task('default', ['sass', 'webserver', 'watch']);
+gulp.task('useref', function(){
+  return gulp.src('development/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('public'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('development/fonts/**/*')
+  .pipe(gulp.dest('public/fonts'))
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:public', 
+    ['sass', 'useref', 'fonts'],
+    callback
+  )
+});
+
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+});
+
+
