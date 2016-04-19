14b871d9b3b112aab6d868c95801110ffe8a9c24
Justin Bennett <fortitude1981@gmail.com>
2016-04-19 18:28:50 -0500

    updated gulp, not working yet

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var runSequence = require('run-sequence');
-
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -35,13 +38,27 @@ gulp.task('useref', function(){
   return gulp.src('development/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('public'))
 });
+
+gulp.task('images', function(){
+  return gulp.src('development/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('public/images'))
+});
+
 
 gulp.task('fonts', function() {
   return gulp.src('development/fonts/**/*')
   .pipe(gulp.dest('public/fonts'))
 });
+
+gulp.task('clean:public', function() {
+  return del.sync('public');
+})
 
 gulp.task('build', function (callback) {
   runSequence('clean:public', 
@@ -49,7 +66,6 @@ gulp.task('build', function (callback) {
     callback
   )
 });
-
 
 gulp.task('default', function (callback) {
   runSequence(['sass','browserSync', 'watch'],
