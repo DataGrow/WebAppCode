6c2176025413967c30c55b7dd9ba23904bf493fe
Larry Clem <larryvincentclem@gmail.com>
2016-04-21 17:17:38 -0500

    included sidebar directive

---
+++ public/development/js/directives/sidebar-dir.js
@@ -0,0 +1,8 @@
+angular.module('dataGrow')
+	.directive('sideBar', function() {
+		return {
+
+		templateUrl: '../../../development/partials/sidebar.html'
+
+	}
+});
--- public/development/js/services/websocketService.js
+++ public/development/js/services/websocketService.js
@@ -1,15 +1,15 @@
-angular.module('DataGrow').service('websocketService', function( $http, $q ) {
+angular.module('dataGrow').service('websocketService', function( $http, $q ) {
 
-    let ws = new WebSocket('ws://localhost:8000');
+    // let ws = new WebSocket('ws://localhost:8000');
 
-    ws.onopen = function() {
-        ws.send("Hello, World!");
-        alert("Message is sent...")
-    };
-
-    ws.onmessage = function (evt) {
-        var received = evt.data;
-        alert("received message: " + received);
-    }
+    // ws.onopen = function() {
+    //     ws.send("Hello, World!");
+    //     alert("Message is sent...")
+    // };
+    //
+    // ws.onmessage = function (evt) {
+    //     var received = evt.data;
+    //     alert("received message: " + received);
+    // }
 
 });
