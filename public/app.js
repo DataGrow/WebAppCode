8f62131a7f86d666d819ad7b3de07c1674292386
Justin Bennett <fortitude1981@gmail.com>
2016-04-21 16:35:52 -0500

    made sidebar directive

--- public/app.js
+++ public/app.js
@@ -32,3 +32,4 @@ angular.module('dataGrow', ['ui.router'])
 						})
 
     });
+
---
+++ public/development/js/dir/sidebar-dir.js
@@ -0,0 +1,8 @@
+angular.module('dataGrow')
+	.directive('sideBar', function() {
+		return {
+
+		templateUrl: './partials/templates/sidebar.html'
+
+	}
+});
